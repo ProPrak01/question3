7475b11974398ed5833db9041e920431710ac3c4
Prakash <127179011+ProPrak01@users.noreply.github.com>
2024-05-01 23:19:28 +0530

    changes

--- src/components/login.js
+++ src/components/login.js
@@ -1,65 +1,67 @@
-import React, { useEffect, useState } from "react"
-import axios from "axios"
-import { useNavigate, Link } from "react-router-dom"
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
 
 function Login() {
+  const history = useNavigate();
 
-    const history=useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [email,setEmail]=useState('')
-    const [password,setPassword]=useState('')
+  async function submit(e) {
+    e.preventDefault();
 
-    async function submit(e){
-        e.preventDefault();
-
-        try{
-
-            await axios.post("https://question3-server.vercel.app/",{
-                email,password
-            })
-            .then(res=>{
-                if(res.data=="exist"){
-                    history("/question3/home",{state:{id:email}})
-                }
-                else if(res.data=="notexist"){
-                    alert("User have not sign up")
-                }
-            })
-            .catch(e=>{
-                alert("wrong details")
-                console.log(e);
-            })
-
-        }
-        catch(e){
-            console.log(e);
-
-        }
-
+    try {
+      await axios
+        .post("https://question3-server.vercel.app/signup", {
+          email,
+          password,
+        })
+        .then((res) => {
+          if (res.data == "exist") {
+            history("/question3/home", { state: { id: email } });
+          } else if (res.data == "notexist") {
+            alert("User have not sign up");
+          }
+        })
+        .catch((e) => {
+          alert("wrong details");
+          console.log(e);
+        });
+    } catch (e) {
+      console.log(e);
     }
+  }
 
+  return (
+    <div className="login">
+      <h1>Login</h1>
 
-    return (
-        <div className="login">
+      <form action="POST">
+        <input
+          type="email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          placeholder="Password"
+        />
+        <input type="submit" onClick={submit} />
+      </form>
 
-            <h1>Login</h1>
+      <br />
+      <p>OR</p>
+      <br />
 
-            <form action="POST">
-                <input type="email" onChange={(e) => { setEmail(e.target.value) }} placeholder="Email"  />
-                <input type="password" onChange={(e) => { setPassword(e.target.value) }} placeholder="Password"  />
-                <input type="submit" onClick={submit} />
-
-            </form>
-
-            <br />
-            <p>OR</p>
-            <br />
-
-            <Link to="/question3/signup">Signup Page</Link>
-
-        </div>
-    )
+      <Link to="/question3/signup">Signup Page</Link>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
